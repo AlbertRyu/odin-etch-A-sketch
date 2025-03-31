7e3684a575b1d24413a892bf3864e68898a5125e
Yunxiao <yunxiao.liu@proton.me>
2025-03-31 17:58:41 +0200

    genGrid and change color after hover

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 console.log("Hello World")
+
+const container = document.querySelector('.container')
+
+
+function genGrid(size){
+    for (let i=0; i<size; i++){
+
+        let row = document.createElement('div')
+        row.classList = 'row'
+
+        for (let j=0; j<=size; j++){
+            const square = document.createElement('div')
+            square.classList = 'square'
+            square.addEventListener('mouseenter',changeColor) 
+
+            row.appendChild(square)
+        }
+        container.appendChild(row)
+    }
+}
+
+function changeColor(event){
+    event.target.style.backgroundColor = 'black'
+}
+
+
+
+genGrid(10)
